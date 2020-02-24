71f0417aee7352a8692cca1e8aa25daf398f1bed
Onur Dağdelen <onurdagdelenn@gmail.com>
2020-02-24 18:48:07 +0300

    First Commit

---
+++ StokTakip/Controllers/MusteriController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokTakip.Models.Entity;
+
+namespace StokTakip.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(string p)
+        {
+            var degerler = from d in db.TBLMUSTERILER select d;
+            if (!string.IsNullOrEmpty(p))
+            {
+
+                degerler = degerler.Where(m => m.musteriad.Contains(p));
+            }
+            return View(degerler.ToList());
+            //var degerler = db.TBLMUSTERILER.ToList();
+           // return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Sil(int id)
+        {
+
+            var musteri = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mus);
+
+
+        }
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.musteriid);
+            musteri.musteriad = p1.musteriad;
+            musteri.musterisoyad = p1.musterisoyad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ StokTakip/Controllers/UrunController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokTakip.Models.Entity;
+
+namespace StokTakip.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+
+                                             {
+                                                 Text = i.kategoriad,
+                                                 Value = i.kategoriid.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLER p1)
+        {
+            var ktg=db.TBLKATEGORILER.Where(m=> m.kategoriid== p1.TBLKATEGORILER.kategoriid).FirstOrDefault(); //kategoriid almak için linq sorgusu yaptım
+            p1.TBLKATEGORILER = ktg;
+
+
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id); //find ile bul remove ile kaldır
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        public ActionResult UrunGetir(int id)
+        {
+
+            var urun = db.TBLURUNLER.Find(id);
+
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+
+                                             {
+                                                 Text = i.kategoriad,
+                                                 Value = i.kategoriid.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View("UrunGetir", urun);
+
+        }
+        public ActionResult Guncelle (TBLURUNLER p)
+        {
+
+            var urun = db.TBLURUNLER.Find(p.urunid);
+            urun.urunad = p.urunad;
+            urun.marka = p.marka;
+            urun.fiyat = p.fiyat;
+            urun.stok = p.stok;
+            //urun.urunkategori = p.urunkategori;
+            var ktg = db.TBLKATEGORILER.Where(m => m.kategoriid == p.TBLKATEGORILER.kategoriid).FirstOrDefault();
+            urun.urunkategori = ktg.kategoriid;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
+
